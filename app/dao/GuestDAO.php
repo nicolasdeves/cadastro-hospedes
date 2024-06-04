e9ad417a6a3912ddce04795d4969aba8dee063f8
Nicolas Deves <nicolas.deves@stwautomacao.com.br>
2024-06-03 23:55:48 -0300

    Feat: adiciona dao

--- app/dao/GuestDAO.php
+++ app/dao/GuestDAO.php
@@ -4,7 +4,8 @@ class GuestDAO {
     private $db;
 
     public function __construct() {
-        $this->db = new SQLite3('../db.db/');
+        $this->db = new SQLite3('C:/Users/nicolas.deves/Desktop/Dev/Projetos/cadastro-hospedes/database/db.db');
+
     }
 
     public function insert($guest) {
@@ -14,7 +15,7 @@ class GuestDAO {
         $stmt->bindValue(':cpf', $guest->getCpf());
         $stmt->bindValue(':phone', $guest->getPhone());
         $stmt->bindValue(':room', $guest->getRoom());
-        $stmt->execute();
+        //$stmt->execute();
 
         return true ? $stmt->execute() : false;
     }
@@ -22,6 +23,7 @@ class GuestDAO {
     public function edit ($guest) {
         $stmt = $this->db->prepare('UPDATE guest SET name = :name, cpf = :cpf, phone = :phone, room = :room WHERE id = :id');
 
+        $stmt->bindValue(':id', $guest->getId());
         $stmt->bindValue(':name', $guest->getName());
         $stmt->bindValue(':cpf', $guest->getCpf());
         $stmt->bindValue(':phone', $guest->getPhone());
@@ -29,28 +31,54 @@ class GuestDAO {
         $stmt->bindValue(':id', $guest->getId());
         $stmt->execute();
 
-        return true ? $stmt->execute() : false;
+        //return true ? $stmt->execute() : false;
     }
 
     public function delete($id) {
-        $stmt = $this->db->prepare('DELETE FROM guest WHERE id = :id');
+        try {
+            $stmt = $this->db->prepare('DELETE FROM guest WHERE id = :id');
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+            return true; // Retorna true se a exclusão foi bem-sucedida
+        } catch (Exception $e) {
+            // Captura e trata qualquer exceção que ocorra durante a execução da consulta
+            echo 'Erro ao excluir: ' . $e->getMessage();
+            return false; // Retorna false se houver um erro
+        }
+    }
+    
+    public function selectById ($id) {
+        $stmt = $this->db->prepare('SELECT * FROM guest WHERE id = :id');
         $stmt->bindValue(':id', $id);
-        $stmt->execute();
+        $result = $stmt->execute();
 
-        return true ? $stmt->execute() : false;
+        $guest = new Guest();
+
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $guest->setId($row['id']);
+            $guest->setName($row['name']);
+            $guest->setCpf($row['cpf']);
+            $guest->setPhone($row['phone']);
+            $guest->setRoom($row['room']);
+        }
+
+        return $guest;
     }
 
     public function selectAll() {
         $stmt = $this->db->prepare('SELECT * FROM guest');
         $result = $stmt->execute();
 
-        $guests = [];
+        $guests = array();
 
         while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $guest = new Guest($row['id'], $row['name'], $row['cpf'], $row['phone'], $row['room']);
+
+            $guest = $this->selectById($row['id']);
+
             array_push($guests, $guest);
         }
 
+        
         return $guests;
     }  
 }
